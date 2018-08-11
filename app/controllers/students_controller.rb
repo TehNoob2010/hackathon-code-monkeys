a407d98fbdb9349479610b5885a670fba218a45e
TehNoob2010 <41767073+TehNoob2010@users.noreply.github.com>
2018-08-11 19:54:06 +1000

    Added index and show for Students API

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,22 @@
 class StudentsController < ApplicationController
-
+    def index
+        @student = Student.all
+        render json: @student
+    end
+    
+    
     def create
         @user = User.find(params[:user_id])
         @student = Student.create({user_id: @user.id, rating: 3.0, rating_count: 1})
     end
 
+    def show
+        student = Student.find(params[:id])
+        user = User.find(student.user_id)
+        render :json => {:user => user, :student => student }
+    end
+
+    
 
 
 end
