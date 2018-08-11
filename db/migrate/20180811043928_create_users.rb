e9476f710c4b7677366d42cb48e8ee5dd102c9e1
Rob <rlainewyllie@gmail.com>
2018-08-11 14:51:42 +1000

    initial user modelling

---
+++ db/migrate/20180811043928_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :s_number
+      t.string :gender
+      
+      t.timestamps
+    end
+  end
+end
