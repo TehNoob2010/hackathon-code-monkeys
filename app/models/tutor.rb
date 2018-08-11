9a50f553cf36d075fc962ab0f424fb9793bbcb8c
Rob <rlainewyllie@gmail.com>
2018-08-11 16:51:00 +1000

    add self reference to rate method in tutor

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,10 +1,10 @@
 class Tutor < ApplicationRecord
   belongs_to :user
-    
+
     def rate(new_rate)
-        rating = ((rating ** rating_count) + new_rate) / (rating_count + 1)
-        rating_count = rating_count + 1
-    end 
-    
-    
+        self.rating = ((self.rating * self.rating_count) + new_rate) / (self.rating_count + 1)
+        self.rating_count = self.rating_count + 1
+    end
+
+
 end
