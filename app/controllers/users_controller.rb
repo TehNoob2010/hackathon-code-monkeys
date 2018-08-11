c1d6f3971a0e0a02d9cf99e2941dc8a249cdf387
Rob <rlainewyllie@gmail.com>
2018-08-12 03:37:44 +1000

    add route to create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,11 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         @user.save
-
+        render json: { :done => "true" }
     end
 
     private
         def user_params
-            params.require(:user).permit(:name, :email, :password, :s_number, :gender)
+            params.permit(:name, :email, :password, :s_number, :gender)
         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,18 @@ Rails.application.routes.draw do
         #get '/tutors/:id/update_rate/:tutor_rate/:course_rate', to: 'tutors#update_rate'
         # get '/students/:id/update_rate/:rating', to: 'students#update_rate'
 
+        post '/user', to: 'users#create'
+
         scope '/tutors/:id' do
             get '/rate', to: 'tutors#update_rate'
         end
 
         scope '/student/:id' do
-            get '/rate', to: 'student#update_rate'
+            get '/rate', to: 'students#update_rate'
         end
 
         # queryParams must contain ?course_name=""&student_id=""&description=""
         post '/start_session', to: 'chat_sessions#create'
-
     end
 
 
