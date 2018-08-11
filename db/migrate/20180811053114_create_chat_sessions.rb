5a088340bf30b64294f450b5a95401e668ed02b7
TehNoob2010 <41767073+TehNoob2010@users.noreply.github.com>
2018-08-11 22:58:43 +1000

    Add linkedIN and SkypeID for tutor :

--- db/migrate/20180811053114_create_chat_sessions.rb
+++ db/migrate/20180811053114_create_chat_sessions.rb
@@ -1,5 +1,6 @@
 class CreateChatSessions < ActiveRecord::Migration[5.2]
   def change
+      
     create_table :chat_sessions do |t|
       t.references :student, foreign_key: true
       t.references :tutor, foreign_key: true
