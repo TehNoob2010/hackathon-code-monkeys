3a5aa728e496edba4e1142e9e543b96bdf23c86b
TehNoob2010 <41767073+TehNoob2010@users.noreply.github.com>
2018-08-11 16:48:37 +1000

    Edit

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,9 @@
 class Course < ApplicationRecord
   belongs_to :tutor
+    
+    def rate(new_rate)
+        rating = ((rating * rating_count) + new_rate) / (rating_count + 1)
+        rating_count = rating_count + 1
+    end     
+    
 end
