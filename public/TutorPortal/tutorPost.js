a61be7fe6e3cca29e877a0cb20f54eb3ce34d7ca
Nathan Williams <npwilliams09@gmail.com>
2018-08-11 17:46:16 +1000

    added API js files
    activeCheck upload and tutorPost.js update

--- public/TutorPortal/tutorPost.js
+++ public/TutorPortal/tutorPost.js
@@ -1,8 +1,18 @@
+const reqID = null;
+const userID = 12353;
 $('#ready').click(()=>{
     let UserID = 189213;
     let postURL = '/api/tutorReady/'+userID;
     $.get(postURL,(data) => {
-        let popUp = "<div class='popup'><h2>Searching for your tutor</h2><div class='loader'>Loading...</div></div>";
-      
+        reqID = data.requestId;
+        let popUp = "<div class='popup' id='reqPopup'><h2>You have a request</h2><div class='userInfo'>"+data.userName+"</div><div class='userInfo>"+data.subject+"</div><div class='userInfo'>"+data.desc+"</div><div><button class='btn btn-secondary' id='accept'>Accept</button><button class='btn btn-secondary' id='decline'>Decline</button></div></div>";
+        $('#contentWrapper').append(popUp);
     });
+});
+$('#accept').click(()=>{
+    $.get('/api/acceptReq?req='+reqID+'&userID='+userID);
+});
+
+$('#decline').click(()=>{
+    $('#reqPopup').remove();
 });
---
+++ public/activeChecker.js
@@ -0,0 +1,4 @@
+//is session active checker
+let sessionID = 1;
+let getURL = '/api/'+sessionID;
+$.get(getURL);
